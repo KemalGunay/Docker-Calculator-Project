29077ee4136cc663f8156733e856235cc670c1d5
kemalgunay <kemalgnay@gmail.com>
2022-03-17 20:32:48 +0300

    Initial commit
    İlk docker uygulamam (hesap makinesi)

---
+++ Tests/test_MathOperations.py
@@ -0,0 +1,40 @@
+import unittest
+
+from MathOperations.addition import Addition
+from MathOperations.subtraction import Subtraction
+from MathOperations.multiplication import Multiplication
+from MathOperations.division import Division
+from MathOperations.exponent import Exponent
+from MathOperations.root import Root
+from MathOperations.log import Log
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_MathOperations_Addition(self):
+        self.assertEqual(3, Addition.sum(1,2))
+
+    def test_MathOperations_Subtraction(self):
+        self.assertEqual(-1, Subtraction.difference(1,2))
+
+    def test_MathOperations_sum_list(self):
+        valuelist = [1,2,3]
+        self.assertEqual(6, Addition.sum(valuelist))
+
+    def test_MathOperations_Multiply(self):
+        self.assertEqual(6, Multiplication.product(2,3))
+
+    def test_calculator_divide(self):
+        self.assertEqual(2, Division.quotient(6,3))
+
+    def test_calculator_root(self):
+        self.assertEqual(13, Root.root(169,2))
+
+    def test_calculator_exponent(self):
+        self.assertEqual(169, Exponent.power(13,2))
+
+    def test_calculator_logarithm(self):
+        self.assertEqual(0, Log.logarithm(10,1))
+
+if __name__ == '__main__':
+    unittest.main()
